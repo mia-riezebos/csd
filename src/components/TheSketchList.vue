2fc8ea836fdaf717a1e307ae8da869d554a5d5f0
mia-cx <mia@mia.cx>
2022-05-10 01:13:18 +0200

    component

---
+++ src/components/TheSketchList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="nav-list">
+    <li><router-link to="/">Home</router-link></li>
+    <li>
+      Sketches
+      <ul class="sketches"></ul>
+    </li>
+  </ul>
+</template>
+
+<style></style>
