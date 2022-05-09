f1825946d5200dab4439156cde6e9dcb4b45d080
mia-cx <mia@mia.cx>
2022-05-10 01:12:49 +0200

    pages

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="about">
+    <h1>csd.mia.cx</h1>
+    <p>
+      CSD, or Creative Systems Design, is a school of programming that uses
+      creative thinking to come up with solutions in an out-of-the-box manner.
+    </p>
+    <p>
+      This website is a collection, or bucket, of projects that I've worked on
+      in my classes on CSD.
+    </p>
+  </section>
+</template>
+
+<style>
+.about {
+  max-width: 640px;
+}
+</style>
---
+++ src/pages/Sketch.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import p5 from "p5";
+import { io } from "socket.io-client";
+
+const modules = import.meta.glob("#/**/*.ts");
+</script>
+
+<template></template>
