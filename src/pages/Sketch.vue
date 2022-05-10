6cd99d311be5cb84846c1ec1bde65a40e79bf060
mia-cx <mia@mia.cx>
2022-05-10 04:27:56 +0200

    actually running the sketches

--- src/pages/Sketch.vue
+++ src/pages/Sketch.vue
@@ -1,8 +1,41 @@
 <script lang="ts">
 import p5 from "p5";
-import { io } from "socket.io-client";
 
 const modules = import.meta.glob("#/**/*.ts");
+
+export default {
+  data() {
+    return {
+      name: null,
+      sketch: null,
+    };
+  },
+
+  methods: {
+    async loadSketch() {
+      this.name = this.$route.params.sketch;
+      this.sketch?.remove();
+      if (!this.name) return;
+      let response = await modules[`../../sketches/${this.name}/sketch.ts`]();
+      this.sketch = new p5(response.default, "sketch");
+    },
+  },
+
+  async created() {
+    this.loadSketch();
+    this.$watch(() => this.$route.params, this.loadSketch);
+  },
+};
+// console.log(this.$route.params.sketch);
 </script>
 
-<template></template>
+<template>
+  <h1>{{ name }}</h1>
+  <div id="sketch"></div>
+</template>
+
+<style>
+#sketch {
+  border-style: dotted;
+}
+</style>
