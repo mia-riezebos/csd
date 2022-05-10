6ba240584086d03c54e00126310d88b2fc5b64d5
mia-cx <mia@mia.cx>
2022-05-10 04:27:21 +0200

    updated base sketches

--- sketches/visual-composition/sketch.ts
+++ sketches/visual-composition/sketch.ts
@@ -1,1 +1,19 @@
-export {};
+import p5 from "p5";
+export default (p: p5) => {
+  console.log("bruh");
+  let x = 100;
+
+  let y = 100;
+
+  p.setup = function () {
+    p.createCanvas(700, 410);
+  };
+
+  p.draw = function () {
+    p.background(0);
+
+    p.fill(255);
+
+    p.rect(x, y, 50, 50);
+  };
+};
