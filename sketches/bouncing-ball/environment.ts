ce32964378a2a640afbbb1bdfb6e986d637c448f
mia-cx <mia@mia.cx>
2022-05-10 04:28:18 +0200

    bouncing balls sketch (proof of concept)

---
+++ sketches/bouncing-ball/environment.ts
@@ -0,0 +1,26 @@
+import sketch from "./sketch";
+
+/* Canvas */
+export let canvasWidth = 700;
+export let canvasHeight = 410;
+
+export let frameRate = 120;
+
+/* Physics */
+export let g = 98.1; // gravitational constant               (0 to disable)
+export let b = 0.9; // bounciness / restituion coëfficient
+export let d = 0; // drag / air restistance               (0 to disable)
+
+/* Object */
+export let amount = 50; // amount of initial balls
+export let size = 50; // average size of initial balls
+export let velocity = 2; // average velocity of initial balls
+
+export let sizeVariance = 3; // variance in size of initial balls
+export let veloVariance = 10; // variance in velocity of inital balls
+
+/* Appearance */
+
+export let ballSaturation = 0.9;
+export let ballLightness = 0.75;
+export let ballOpacity = 0.3;
---
+++ sketches/bouncing-ball/object.ts
@@ -0,0 +1,168 @@
+import * as env from "./environment";
+import p5 from "p5";
+
+export let click = false;
+
+export class Canvas {
+  p: p5;
+  w: number;
+  h: number;
+  f: number;
+
+  constructor(sketch: p5, w?: number, h?: number, f?: number) {
+    this.p = sketch;
+    if (!w) {
+      this.w = env.canvasWidth;
+    } else {
+      this.w = w;
+    }
+    if (!h) {
+      this.h = env.canvasHeight;
+    } else {
+      this.h = h;
+    }
+    if (!f) {
+      this.f = env.frameRate;
+    } else {
+      this.f = f;
+    }
+  }
+
+  create() {
+    this.p.colorMode(this.p.HSL, 360, 1, 1, 1);
+    this.p.createCanvas(this.w, this.h);
+    this.fill(0, 0, 0.1, 1);
+    this.p.frameRate(this.f);
+  }
+
+  fill(h: number, s: number, l: number, a: number) {
+    this.p.background(h, s, l, a);
+  }
+
+  resize() {
+    let w = document.documentElement.clientWidth;
+    let h = document.documentElement.clientHeight;
+
+    this.p.resizeCanvas(w, h);
+  }
+}
+
+export class Ball {
+  p: p5;
+  d: number;
+  x: number;
+  y: number;
+  vX: number;
+  vY: number;
+  m: number;
+  uuid: string;
+
+  constructor(
+    sketch: p5,
+    d?: any,
+    x?: any,
+    y?: any,
+    vX?: any,
+    vY?: any,
+    m?: any,
+    uuid?: string
+  ) {
+    this.p = sketch;
+    if (!d) {
+      this.d =
+        (env.size +
+          env.size * (env.sizeVariance * Math.random()) -
+          env.sizeVariance) >>
+        1;
+    } else {
+      this.d = d;
+    }
+    if (!x) {
+      this.x = this.d / 2 + (env.canvasWidth - this.d) * Math.random();
+    } else {
+      this.x = x;
+    }
+    if (!y) {
+      this.y = this.d / 2 + (env.canvasHeight - this.d) * Math.random();
+    } else {
+      this.y = y;
+    }
+    if (!vX) {
+      (this.vX =
+        env.velocity *
+          (env.veloVariance * Math.random() - env.veloVariance / 2) -
+        env.velocity / 2) / env.frameRate;
+    } else {
+      this.vX = vX;
+    }
+    if (!vY) {
+      (this.vY =
+        env.velocity *
+          (env.veloVariance * Math.random() - env.veloVariance / 2) -
+        env.velocity / 2) / env.frameRate;
+    } else {
+      this.vY = vY;
+    }
+    if (!m) {
+      this.m = this.d + this.d * Math.random();
+    } else {
+      this.m = m;
+    }
+    if (!uuid) {
+      this.uuid = Math.random().toString(36).substr(2, 9);
+    } else {
+      this.uuid = uuid;
+    }
+  }
+
+  hue = Math.floor(Math.random() * 360);
+  saturation = env.ballSaturation;
+  lightness = env.ballLightness;
+  alpha = env.ballOpacity;
+
+  g = env.g / env.frameRate;
+
+  translate(canvas: { h: number }) {
+    if (this.y + this.d / 2 < canvas.h) {
+      this.vY += this.g;
+    }
+
+    this.x += this.vX;
+    this.y += this.vY;
+  }
+
+  collide(obj: { w: number; h: number }) {
+    if (obj instanceof Canvas) {
+      if (
+        (this.x + this.d / 2 >= obj.w && this.vX >= 0) ||
+        (this.x - this.d / 2 < 0 && this.vX < 0)
+      ) {
+        this.vX = -this.vX * env.b;
+      }
+
+      if (
+        (this.y + this.d / 2 >= obj.h && this.vY >= 0) ||
+        (this.y - this.d / 2 < 0 && this.vY < 0)
+      ) {
+        this.vY = -this.vY * env.b;
+      }
+    } else {
+      // TODO object collision
+    }
+  }
+
+  render(canvas: any) {
+    this.p.colorMode(this.p.HSL, 360, 1, 1, 1);
+    this.p.noStroke();
+    this.p.fill(this.hue, this.saturation, this.lightness, this.alpha);
+
+    this.p.ellipse(this.x, this.y, this.d);
+
+    this.collide(canvas);
+    this.translate(canvas);
+  }
+
+  nSqrt(n: number) {
+    return n > 0 ? this.p.sqrt(Math.abs(n)) : -this.p.sqrt(Math.abs(n));
+  }
+}
