3c92399e12d7ec216d2ff613f58e7bec39acf1dd
mia-cx <mia@mia.cx>
2022-05-11 23:20:01 +0200

    update title & bouncing ball env

--- sketches/bouncing-ball/environment.ts
+++ sketches/bouncing-ball/environment.ts
@@ -8,13 +8,13 @@ export let frameRate = 120;
 
 /* Physics */
 export let g = 98.1; // gravitational constant               (0 to disable)
-export let b = 0.9; // bounciness / restituion coëfficient
+export let b = 0.85; // bounciness / restituion coëfficient
 export let d = 0; // drag / air restistance               (0 to disable)
 
 /* Object */
-export let amount = 50; // amount of initial balls
-export let size = 50; // average size of initial balls
-export let velocity = 2; // average velocity of initial balls
+export let amount = 20; // amount of initial balls
+export let size = 75; // average size of initial balls
+export let velocity = 3; // average velocity of initial balls
 
 export let sizeVariance = 3; // variance in size of initial balls
 export let veloVariance = 10; // variance in velocity of inital balls
