d39809a8919b7b535aeb12f9c2ee947388a53718
mia-cx <mia@mia.cx>
2022-07-05 13:39:26 +0200

    dynamic resolution

--- sketches/visual-composition/env.ts
+++ sketches/visual-composition/env.ts
@@ -1,21 +1,38 @@
 import p5 from "p5";
+// import p from "./p";
 
 const CANVAS = {
+  REDRAW: false,
   WIDTH: 1024,
   HEIGHT: 1024,
   FRAMERATE: 144,
-  RESOLUTION: 16,
+  BASE_RESOLUTION: 16,
+  RESOLUTION: function (r?: number) {
+    if (!r) return this.value;
+    this.value = r;
+    CANVAS.PIXEL_RATIO = this.value ** -1;
+    [CANVAS.COLUMNS, CANVAS.ROWS] = [
+      Math.floor(CANVAS.WIDTH * CANVAS.PIXEL_RATIO),
+      Math.floor(CANVAS.HEIGHT * CANVAS.PIXEL_RATIO),
+    ];
+  },
   PIXEL_RATIO: 0,
   COLUMNS: 0,
   ROWS: 0,
 };
 
-CANVAS.PIXEL_RATIO = CANVAS.RESOLUTION ** -1;
+CANVAS.RESOLUTION(CANVAS.BASE_RESOLUTION);
 
 [CANVAS.COLUMNS, CANVAS.ROWS] = [
   Math.floor(CANVAS.WIDTH * CANVAS.PIXEL_RATIO),
   Math.floor(CANVAS.HEIGHT * CANVAS.PIXEL_RATIO),
 ];
+
+export let COLOR: {
+  PHASE_SHIFT: p5.Element;
+} = {
+  PHASE_SHIFT: null,
+};
 
 export { CANVAS };
 
