37deb1424e00ebd53af65b29849a7049c6ed6bcd
mia-cx <mia@mia.cx>
2022-07-14 01:07:22 +0200

    simple sketches landing page

---
+++ src/routes/sketches.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>sketches</title>
+</svelte:head>
+
+<section class="about">
+  <h1>Sketches</h1>
+  <p>
+    Below is a list of p5js sketches I have made for my CSD classes
+  </p>
+</section>
